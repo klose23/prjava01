b4205a95d393578445cd00e52c56b0932c261112
Adri <Adri@Adri-PC>
2014-04-21 21:21:25 +0200

    Versió 0.2  del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -29,7 +29,7 @@ public static void main(String[]args)throws IOException{
 		bw.write("</title>");bw.newLine(); 
 		bw.write("</head>");bw.newLine(); 
 		bw.write("<body>");bw.newLine(); 
-		bw.write("Novap&agrave;ginaweb");bw.newLine(); 
+		bw.write("<h1>DAW2m08uf4pr2</h1>");bw.newLine(); 
 		bw.write("</body>");bw.newLine(); 
 		bw.write("</html>");bw.newLine(); 
 		bw.close(); 
